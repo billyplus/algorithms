156773a3a5ce29af6470fdfc22bf3495bac368bc
billy <me@ybilly.com>
2020-06-21 23:09:56 +0800

    update

--- 8puzzle/src/Solver.java
+++ 8puzzle/src/Solver.java
@@ -1,23 +1,61 @@
+import java.util.ArrayList;
+
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
+    private final int minMoves;
+
+    private class Node {
+        public final Board node;
+        public final Board parent;
+
+        // private final int step;
+        public Node(Board b, Board parent) {
+            node = b;
+            this.parent = parent;
+            // this.step = step;
+        }
+
+    }
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
-        throw new IllegalArgumentException();
+        ArrayList<Node> steps = new ArrayList<Node>();
+        ArrayList<Node> unreachable = new ArrayList<Node>();
+        MinPQ<Node> solveTree = new MinPQ<Node>();
+        solveTree.insert(new Node(initial, null));
+        boolean skip = false;
+        while (!solveTree.isEmpty()) {
+            skip = false;
+            Node current = solveTree.delMin();
+            for (Node n : solveTree) {
+                if (current.node.equals(n.node)) {
+                    skip = true;
+                }
+            }
+            if (skip) {
+                continue;
+            }
+            steps.add(current);
+            if (current.isGoal()) {
+
+            }
+            for (Board n : current.neighbors()) {
+                solveTree.insert(n);
+            }
+        }
     }
 
     // is the initial board solvable? (see below)
     public boolean isSolvable() {
-        throw new IllegalArgumentException();
-
+        return minMoves != -1;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
-        throw new IllegalArgumentException();
-
+        return minMoves;
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
